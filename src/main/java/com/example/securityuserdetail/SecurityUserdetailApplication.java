cf79b4ab6d6c280489cc88611b0121fc4ec68cad
Izai Vanegas <izai.vanegas@gmail.com>
2023-12-04 21:57:58 -0600

    Se suben los cambios finales

---
+++ src/main/java/com/example/securityuserdetail/SecurityUserdetailApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityuserdetail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityUserdetailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityUserdetailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/securityuserdetail/SecurityUserdetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityuserdetail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityUserdetailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
